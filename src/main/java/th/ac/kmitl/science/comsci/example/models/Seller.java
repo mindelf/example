2b749308cd904d26a29729020202cd706169cd47
Pasthiyakan <mind_mermaid@hotmail.com>
2017-11-24 10:33:13 +0700

    Add Seller Class to model

---
+++ src/main/java/th/ac/kmitl/science/comsci/example/models/Seller.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package seller;
+
+/**
+ *
+ * @author Mirana
+ */
+public class Seller {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    private String sellerID;
+    private String sellerGlobalID;
+    private String sellerName;
+
+
+    public String getSellerID() {
+        return sellerID;
+    }
+
+    public void setSellerID(String sellerID) {
+        this.sellerID = sellerID;
+    }
+    
+    public String getSellerGlobalID() {
+        return sellerGlobalID;
+    }
+
+    public void setSellerGlobalID(String sellerGlobalID) {
+        this.sellerGlobalID = sellerGlobalID;
+    }
+
+    public String getSellerName() {
+        return sellerName;
+    }
+
+    public void setSellerName(String sellerName) {
+        this.sellerName = sellerName;
+    }
+
+    
+}
